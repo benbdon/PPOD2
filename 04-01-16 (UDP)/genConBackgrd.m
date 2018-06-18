06dd2fa0ac0759c409a0d7ff5c09874d96662a09
Ben Don <benjamindon2018@u.northwestern.edu>
2018-06-18 15:43:51 -0500

    test program now generates continously (non-blocking) and acquires for 2 seconds (blocking)

--- 04-01-16 (UDP)/genConBackgrd.m
+++ 04-01-16 (UDP)/genConBackgrd.m
@@ -3,33 +3,32 @@ daqreset
 
 %Initialize DAQ settings
 s = daq.createSession('ni');
-%sAI = daq.createSession('ni');
+sAI = daq.createSession('ni');
 s.Rate=1000;
-%sAI.Rate=10000;
+sAI.Rate=1000;
+sAI.DurationInSeconds = 2;
 
 ch = addAnalogOutputChannel(s,'Dev1', 'ao6', 'Voltage');
 ch.Name = 'Command Signal';
 
-% ch = addAnalogInputChannel(sAI, 'Dev2', 'ai23', 'Voltage');
-% ch.Name = 'Measurement Signal';
-% ch.TerminalConfig = 'SingleEnded';
+ch = addAnalogInputChannel(sAI, 'Dev2', 'ai23', 'Voltage');
+ch.Name = 'Measurement Signal';
+ch.TerminalConfig = 'SingleEnded';
 
 %Add listener to the 'DataRequired' event and assign it to the variable lh
 %lhAI = addlistener(sAI,'DataAvailable', @plotData);
 lhAO = addlistener(s,'DataRequired',@queueMoreData);
-s.IsContinuous = true;
+s.IsContinuous = true; %NECESSARY AND NOT IN DOCUMENTATION
 
 %Queue sample data
-queueOutputData(s,linspace(0,5,5000)');
+queueOutputData(s,linspace(0,5,999)');
 
 %Start AI/AO tasks
 startBackground(s);
 
 %await keypress
 pause();
-%data = startForeground(sAI);
-
-%delete listener
-% pause();
-% plot(data)
+disp(s.ScansQueued)
+measured = startForeground(sAI);
 delete(lhAO)
+plot(measured)
--- 04-01-16 (UDP)/queueMoreData.m
+++ 04-01-16 (UDP)/queueMoreData.m
@@ -1,3 +1,3 @@
 function queueMoreData(src,event)
-    queueOutputData(src, linspace(1,5,5000)');
+    queueOutputData(src, linspace(1,5,5*999)'); %documentation shows s rather than src, but that's wrong.
 end
