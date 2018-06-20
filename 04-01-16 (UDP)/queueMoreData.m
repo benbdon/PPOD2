185bfe63cd42719d64c9a71f4c764db95cdd98f2
Ben Don <benjamindon2018@u.northwestern.edu>
2018-06-20 17:18:23 -0500

    Removed some unnecessary files.

--- 04-01-16 (UDP)/queueMoreData.m
+++
@@ -1,3 +0,0 @@
-function queueMoreData(src,event)
-    queueOutputData(src, linspace(1,5,5*999)'); %documentation shows s rather than src, but that's wrong.
-end
--- 04-01-16 (UDP)/triggered_loopback.m
+++ 04-01-16 (UDP)/triggered_loopback.m
@@ -1,10 +1,4 @@
-%This function does a loopback test using a stairstep from 0 - 5
-% as an output from ao6 and measures it back on channel ai23. 
-
-%FYI: despite starting the the AO/AI task at the same time, the AO leads
-%the AI by ~45 mSec
-
-%reset all DAQ settings (just in case)
+%This program outputs a continouos output signal 0 - 5V on ao6
 daqreset
 
 %'Dev2' - PCIe-6323 - AI
@@ -13,35 +7,38 @@ sAI.Rate = 1000;
 ch = addAnalogInputChannel(sAI, 'Dev2', 'ai23', 'Voltage');
 ch.Name = 'Measurement Signal';
 ch.TerminalConfig = 'SingleEnded';
-
 addTriggerConnection(sAI,'External','Dev2/PFI0','StartTrigger');
 sAI.Connections(1).TriggerCondition = 'RisingEdge';
 
+
+
+
+
+
 %'Dev1' - PCI-6713 - AO
 sAO = daq.createSession('ni');
-sAO.Rate = 10000;
+sAO.Rate = 1000;
+lhAO = addlistener(sAO,'DataRequired', ...
+    @(src,event) src.queueOutputData([clock, signal]));
+sAO.IsContinuous = true; %NECESSARY AND NOT IN DOCUMENTATION
 
 ch = addAnalogOutputChannel(sAO,'Dev1', 'ao0', 'Voltage');
 ch.Name = 'clock';
-clock = zeros(2000,1);
-clock(1000,1) = 5;
+clock = zeros(5000,1);
+clock(250,1) = 5;
 
 ch = addAnalogOutputChannel(sAO,'Dev1', 'ao6', 'Voltage');
 ch.Name = 'signal';
-signal = [linspace(0,1,1999)';0];
+signal = [linspace(0,5,4999)';0];
 
 %Queue up the output
 queueOutputData(sAO,[clock,signal])
-startBackground(sAO);
-[measured_data,timestamps] = startForeground(sAI);
 
-plot(timestamps+1,measured_data)
-hold on
-time = linspace(0,2,2000)';
-plot(time,signal)
-xlabel('Time');
-ylabel('Voltage');
-legend('measured data','signal')
-stop(sAO);
-stop(sAI);
-disp('Done')
+%Start AI/AO tasks
+startBackground(sAO);
+pause(); %await keypress
+[data,timestamps] = startForeground(sAI);
+delete(lhAO)
+stop(sAO)
+stop(sAI)
+plot(timestamps,data)
